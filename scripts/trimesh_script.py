04bd18c57e13570b21326bdca7794c40a984652a
gabosiles <gabosiles23@gmail.com>
2025-02-21 16:09:02 +0100

    Adding voxel to xml code and structure

--- scripts/trimesh_script.py
+++
@@ -1,7 +0,0 @@
-
-from trimesh_coding.trimesh_coding import trimesh_voxelization
-
-if __name__ == '__main__':
-    input_path = "../resources/input/voxelization/IAIDrawerW60H26.stl"
-    output_path = "../resources/output/voxelized_mesh.stl"
-    trimesh_voxelization(input_path,output_path)
--- src/trimesh_coding/trimesh_coding.py
+++
@@ -1,7 +0,0 @@
-import trimesh
-def trimesh_voxelization(input_stl_file: str,output_stl_file: str):
-    mesh = trimesh.load_mesh(input_stl_file)
-    resolution = 64
-    voxelized_mesh = mesh.voxelized(pitch=mesh.bounding_box.extents.max() / resolution)
-    voxel_mesh = voxelized_mesh.as_boxes()
-    voxel_mesh.export(output_stl_file)
